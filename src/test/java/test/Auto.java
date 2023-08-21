447a3f5155eb74bb51ca8f29051bcb78acded779
daninjaa <dagarciat@unal.edu.co>
2023-08-20 23:32:41 -0500

    Corrección

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -1,30 +1,65 @@
 package test;
 
 public class Auto {
+
 	String modelo;
+	
 	int precio;
-	Asiento [] asientos; 
+	
+	Asiento [] asientos;
+	
 	String marca;
+	
 	Motor motor;
+	
 	int registro;
+	
 	static int cantidadCreados;
 	
+	
+	
 	int contarAsientos(){
+	
 		int contador = 0;
-		for (Asiento silla : this.asientos) {
-		     contador++;
-		}	
+		
+		for (int i = 0; i < this.asientos.length; i++) {
+		
+			if (asientos[i] != null) {
+			
+			contador ++;
+		
+			}
+		
+		}
+		
 		return contador;
+		
+	}
+		
+	
+	String verificarIntegridad(){
+	
+		for (int i = 0; i < this.asientos.length; i++){
+		
+			if (this.asientos[i].registro != this.registro){
+			
+				return "Las piezas no son originales";
+			
+			}
+		
 		}
-	String verificarIntegridad(Motor numMotor, Asiento numAsiento, Auto numAuto) {
-		if (numMotor.registro != numAsiento.registro && numAsiento.registro != numAuto.registro) {
+		
+		if (this.registro != this.motor.registro){
+		
 			return "Las piezas no son originales";
+		
 		}
+		
 		else {
+		
 			return "Auto original";
-		  }
 		
+		}
+	
 	}
-
-		
-	}
+}
--- src/test/java/test/Motor.java
+++ src/test/java/test/Motor.java
@@ -1,16 +1,5 @@
 package test;
 
 public class Motor {
-	int numeroCilindros;
-	String tipo;
-	int registro;
-	
-	void cambiarRegistro (int registro) {
-		this.registro = registro;
-	}
-	void asignarTipo (String tipo) {
-	    if (tipo == "gasolina" || tipo == "electrico") {
-			this.tipo = tipo;
-	    }
-	}
+
 }
